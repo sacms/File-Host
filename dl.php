83ae22acfe9d7b67ac2987ffc87dafc1f8ad67c1
Shakib Ahmed <87657683+sacms@users.noreply.github.com>
2022-06-19 22:37:10 +0600

    Update dl.php

--- dl.php
+++ dl.php
@@ -3,11 +3,10 @@
 if (!empty($_GET['key'])) {
 if (!empty($_GET['fid'])) {
 
-$uri = base64_decode('aHR0cHM6Ly9kbC5zaGFraWIuY3lvdS9kbC5waHA=');
 
 $fid = $_GET['fid'];
 $key = $_GET['key'];
-header('Location: '.$uri.'?fid='.$fid.'&key='.$key.'');
+header('Location: https://dl.shakib.cyou/dl.php?fid='.$fid.'&key='.$key.'');
 } else {
 header('Location: /');
 }
