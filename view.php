f5412862975415af84b24afe063eac8da1a0393b
Shakib Ahmed <87657683+sacms@users.noreply.github.com>
2022-06-21 10:50:05 +0600

    Update view.php

--- view.php
+++ view.php
@@ -5,7 +5,21 @@ if (!empty($_GET['id'])) {
 
 $id = $_GET['id'];
 
- $con = file_get_contents('https://dl.shakib.cyou/api/json.php?id='.$id);
+function sacms($url) {
+    $agent= 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.0.3705; .NET CLR 1.1.4322)';
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_VERBOSE, true);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_USERAGENT, $agent);
+    curl_setopt($ch, CURLOPT_URL,$url);
+
+    return curl_exec($ch);
+
+    curl_close($ch);
+}
+
+ $con = sacms('https://dl.shakib.cyou/api/json.php?id='.$id);
 
  $data = json_decode($con); 
 
