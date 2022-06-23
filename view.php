f28e49926c9a5eaafc3343c09de7668b61201dc8
Shakib Ahmed <87657683+sacms@users.noreply.github.com>
2022-06-23 18:13:10 +0600

    Update view.php

--- view.php
+++ view.php
@@ -4,8 +4,8 @@
 if (!empty($_GET['id'])) {
 
 $id = $_GET['id'];
-
- $con = file_get_contents('https://dl.shakib.cyou/api.php?json&id='.$id);
+$uri = base64_decode('aHR0cHM6Ly9kbC5zaGFraWIuY3lvdS9hcGkucGhwP2pzb24=');
+ $con = file_get_contents($uri.'&id='.$id);
 
  $data = json_decode($con); 
 
