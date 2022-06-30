1f8e2c9fcd2b5759fd2e231a406444b166e91530
Shakib Ahmed <87657683+sacms@users.noreply.github.com>
2022-06-30 12:26:09 +0600

    Update view.php

--- view.php
+++ view.php
@@ -1,11 +1,25 @@
 <?php
 
+function sacms($url) {
+    $agent= 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.0.3705; .NET CLR 1.1.4322)';
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_VERBOSE, true);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_USERAGENT, $agent);
+    curl_setopt($ch, CURLOPT_URL,$url);
+
+    return curl_exec($ch);
+
+    curl_close($ch);
+}
+
 
 if (!empty($_GET['id'])) {
 
 $id = $_GET['id'];
 $uri = base64_decode('aHR0cHM6Ly9kbC5zaGFraWIuY3lvdS9hcGkucGhwP2pzb24=');
- $con = file_get_contents($uri.'&id='.$id);
+ $con = sacms($uri.'&id='.$id);
 
  $data = json_decode($con); 
 
