67f6936f2a59fd0aca10307a833ec4debb6242d8
Shakib Ahmed <87657683+sacms@users.noreply.github.com>
2022-06-30 12:54:48 +0600

    Update embed.php

--- embed.php
+++ embed.php
@@ -3,7 +3,7 @@
 if(!empty($_GET['id'])) {
 $get = $_GET['id'];
 
-$uri = base64_decode('aHR0cHM6Ly9kbC5zaGFraWIuY3lvdS9zdHJlYW0v');
+$uri = base64_decode('aHR0cHM6Ly9kbC5zaGFraWIuY3lvdS9lbWJlZC8=');
 
 echo file_get_contents($uri.$get);
 
