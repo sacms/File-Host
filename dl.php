004807edb1f18b7a504ce0958303b229f9294726
Shakib Ahmed <87657683+sacms@users.noreply.github.com>
2022-12-12 22:07:38 +0600

    Update dl.php

--- dl.php
+++ dl.php
@@ -6,7 +6,7 @@ if (!empty($_GET['fid'])) {
 
 $fid = $_GET['fid'];
 $key = $_GET['key'];
-  $url = base64_decode('aHR0cHM6Ly9kbC5zaGFraWIuY3lvdS9kbC5waHA/');
+  $url = base64_decode('aHR0cHM6Ly9taXlhZHJpdmUubXkuaWQvZGwucGhwPw==');
 header('Location: '.$url.'fid='.$fid.'&key='.$key.'');
 } else {
 header('Location: /');
