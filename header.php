894afb71da8f277b0f5bbcbb6635e057624c22df
Shakib Ahmed <87657683+sacms@users.noreply.github.com>
2022-12-16 12:37:55 +0600

    Update header.php

--- header.php
+++ header.php
@@ -19,9 +19,9 @@ $title = $name.' | '.$sitename;
   <meta property="og:url" content="<?= 'https://'.$_SERVER['SERVER_NAME']; ?>" />
   <link rel="icon" type="image/x-icon" href="https://sb-admin-pro.startbootstrap.com/assets/img/favicon.png" />
   <meta property="og:image" content="https://sb-admin-pro.startbootstrap.com/assets/img/favicon.png" />
-  <link rel="stylesheet" href="https://dl.shakib.cyou/assets/css/bootstrap.min.css" type="text/css" />
-  <link rel="stylesheet" href="https://dl.shakib.cyou/assets/fonts/css/font-awesome.min.css">
-  <link rel="stylesheet" href="https://dl.shakib.cyou/assets/css/style.css" type="text/css" />
+  <link rel="stylesheet" href="https://sambd.top/assets/css/bootstrap.min.css" type="text/css" />
+  <link rel="stylesheet" href="https://sambd.top/assets/fonts/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://sambd.top/assets/css/style.css" type="text/css" />
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <style>			#text-url{width:80%;}			#av-btn{margin-left:10%;}			@media (max-width: 700px) { 				#text-url{width:100%;}				#av-btn{margin-left:0%;}			}		</style>
 </head>
