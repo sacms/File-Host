63fdf39ea5c928b4b788d0db6ce5710522074c89
Shakib Ahmed <87657683+sacms@users.noreply.github.com>
2022-12-27 09:55:58 +0600

    Update footer.php

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
   <a class="head-btn" href="/policy.php">Privacy Policy</a> 
   <a class="head-btn" href="/dmca.php">DMCA</a>  </div> </div>	
 <script src="/assets/js/popper.min.js"></script>	
-<script src="https://sambd.top/assets/js/jquery-1.9.1.min.js"></script>		
-<script src="https://sambd.top/assets/js/bootstrap.min.js"></script>		
-<script src="https://sambd.top/assets/js/bootstrap.bundle.min.js"></script>		
+<script src="/assets/js/jquery-1.9.1.min.js"></script>		
+<script src="/assets/js/bootstrap.min.js"></script>		
+<script src="/assets/js/bootstrap.bundle.min.js"></script>		
 </body>	</html>
